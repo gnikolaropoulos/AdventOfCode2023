ce04960275c9bb9dbbebd742b35890655a658699
George <g.nikolaropoulos@gmail.com>
2023-12-12 00:35:19 +0200

    add day 10 part 2

--- day10/main.py
+++ day10/main.py
@@ -16,45 +16,79 @@ def solve(coords, grid):
     path = [coords]
     while next_coord:
         internal_next_coord = []
-        for x, y in next_coord:
-            ch = grid[x][y]
+        for row, col in next_coord:
+            ch = grid[row][col]
             nbs = []
             if ch == '|':
-                nbs = [(x - 1, y), (x + 1, y)]
+                nbs = [(row - 1, col), (row + 1, col)]
             elif ch == '-':
-                nbs = [(x, y - 1), (x, y + 1)]
+                nbs = [(row, col - 1), (row, col + 1)]
             elif ch == 'L':
-                nbs = [(x - 1, y), (x, y + 1)]
+                nbs = [(row - 1, col), (row, col + 1)]
             elif ch == 'J':
-                nbs = [(x - 1, y), (x, y - 1)]
+                nbs = [(row - 1, col), (row, col - 1)]
             elif ch == '7':
-                nbs = [(x + 1, y), (x, y - 1)]
+                nbs = [(row + 1, col), (row, col - 1)]
             elif ch == 'F':
-                nbs = [(x + 1, y), (x, y + 1)]
-            for nx, ny in nbs:
-                if grid[nx][ny] == 'S':
+                nbs = [(row + 1, col), (row, col + 1)]
+            for n_row, n_col in nbs:
+                if grid[n_row][n_col] == 'S':
                     found_start = True
-                elif (nx, ny) not in visited:
-                    visited.add((nx, ny))
-                    internal_next_coord.append((nx, ny))
-                    path.append((nx, ny))
+                elif (n_row, n_col) not in visited:
+                    visited.add((n_row, n_col))
+                    internal_next_coord.append((n_row, n_col))
+                    path.append((n_row, n_col))
         next_coord = internal_next_coord
     return found_start, path
 
 
+def find_enclosed_points(path, grid):
+    total = 0
+    for row in range(len(grid)):
+        crossing_loop_count = 0
+        last_corner = ""
+        for column in range(len(grid[0])):
+            ch = grid[row][column]
+            if (row, column) in path:
+                match ch:
+                    case "|":
+                        crossing_loop_count += 1
+                    case "L":
+                        last_corner = "L"
+                    case "F":
+                        last_corner = "F"
+                    case "7":
+                        if last_corner == "L":
+                            crossing_loop_count += 1
+                    case "J":
+                        if last_corner == "F":
+                            crossing_loop_count += 1
+            else:
+                if crossing_loop_count % 2 != 0:
+                    total += 1
+    return total
+
+
 def solve_part_1(puzzle_input):
-    start_x, start_y = find_start(puzzle_input)
-    for neighbour_x, neighbour_y in find_neighbours(start_x, start_y):
-        ok, path = solve((neighbour_x, neighbour_y), puzzle_input)
+    start_row, start_col = find_start(puzzle_input)
+    for neighbour_row, neighbour_col in find_neighbours(start_row, start_col):
+        ok, path = solve((neighbour_row, neighbour_col), puzzle_input)
         if ok:
             break
-    path.append((start_x, start_y))
+    path.append((start_row, start_col))
 
     return len(path) // 2
 
 
 def solve_part_2(puzzle_input):
-    pass
+    start_row, start_col = find_start(puzzle_input)
+    for neighbour_row, neighbour_col in find_neighbours(start_row, start_col):
+        ok, path = solve((neighbour_row, neighbour_col), puzzle_input)
+        if ok:
+            break
+    path.append((start_row, start_col))
+    puzzle_input[start_row] = puzzle_input[start_row].replace("S", "J")
+    return find_enclosed_points(path, puzzle_input)
 
 
 def get_puzzle_input():
